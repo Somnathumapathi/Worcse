45d12f5d250539ccb06f84077e16636f8aa8fdce
Manas Hejmadi <manas2all@outlook.com>
2024-01-23 18:13:28 +0530

    final changes

--- streampay/app/DC/dcHomePage/page.js
+++ streampay/app/DC/dcHomePage/page.js
@@ -21,63 +21,64 @@ const dc = () => {
     }, []);
 
     useEffect(() => {
-        if(currentUser!=null) {
-        const fetchProjects = async () => {
-    try{
-        
-            const q = query(collection(db, 'projects'), where('dcId', '==', currentUser.uid));
-        const snap = await getDocs(q);
-        const projectsData = snap.docs.map((doc) => ({
-            id: doc.id,
-            ...doc.data()
-        }));
-        setMyProjects(projectsData)
-        console.log('Fetched', currentUser.uid)
-        
-       
-        
-    } catch (e) {
-        console.log('Error occured: ', e)
-    }
-        }   
-        fetchProjects()
-    }
+        if (currentUser != null) {
+            const fetchProjects = async () => {
+                try {
+
+                    const q = query(collection(db, 'projects'), where('dcId', '==', currentUser.uid));
+                    const snap = await getDocs(q);
+                    const projectsData = snap.docs.map((doc) => ({
+                        id: doc.id,
+                        ...doc.data()
+                    }));
+                    setMyProjects(projectsData)
+                    console.log('Fetched', currentUser.uid)
+
+
+
+                } catch (e) {
+                    console.log('Error occured: ', e)
+                }
+            }
+            fetchProjects()
+        }
 
     }, [currentUser])
     const handleLogOut = async () => {
-        try{
-          await signOut(auth)
-          console.log('Logged out')
-          window.location.href='/DC'
+        try {
+            await signOut(auth)
+            console.log('Logged out')
+            window.location.href = '/DC'
         } catch (e) {
-          console.log('Error: ', e)
+            console.log('Error: ', e)
         }
-      }
-  return (
-    <div>
-        <h1 className='text-3xl text-center'>DC</h1>
-        <button><a href='/DC/Initiate'>Initiate Project</a></button> <br/>
-        <button>My Projects</button><br/>
-        {myProjects.map((project)=> { return (
-                <div key={project.id}>
-                <div className="pt-2 transform transition-transform bg-blue-900 rounded-md pl-4 m-4">
+    }
+    return (
+        <div>
+            <h1 className='text-3xl text-center'>DC</h1>
+            <button><a href='/DC/Initiate'>Initiate Project</a></button> <br />
+            <button>My Projects</button><br />
+            {myProjects.map((project) => {
+                return (
+                    <div key={project.id}>
+                        <div className="pt-2 transform transition-transform bg-blue-900 rounded-md pl-4 m-4">
 
-<p className="font-bold font-sans hover:text-blue-500">{project.projectName}</p>
-<div className="text-gray-300 font-semibold font-sans">
-<p className="">DC ID: 753Y629838</p>
-<p>TOTAL FUNDS: 1000</p>
-<p className="pb-3">ProjectStatus: In progress</p>
+                            <p className="font-bold font-sans hover:text-blue-500">{project.projectName}</p>
+                            <div className="text-gray-300 font-semibold font-sans">
+                                <p className="">DC ID: {project.dcId}</p>
+                                <p>TOTAL FUNDS: {project.funds}</p>
+                                <p className="pb-3">ProjectStatus: {project.completionStatus}</p>
 
 
-</div>
-</div>
-            </div>
-            )
-                
+                            </div>
+                        </div>
+                    </div>
+                )
+
             })}
-        <button onClick={handleLogOut} className='bg-blue-500 rounded-lg p-2'>Log out</button>
-    </div>
-  )
+            <button onClick={handleLogOut} className='bg-blue-500 rounded-lg p-2'>Log out</button>
+        </div>
+    )
 }
 
 export default dc
